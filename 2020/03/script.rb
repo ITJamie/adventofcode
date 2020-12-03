a5b73f4e7d5c5f3c7497d59f84e62a86c134e5fe
Jamie (Bear) Murphy <jamiemurphyit@gmail.com>
2020-12-03 11:35:59 +0000

    day 3 complete

--- 2020/03/script.rb
+++ 2020/03/script.rb
@@ -13,21 +13,52 @@ File.open('input.txt').each do |line|
   lines << new_line
 end
 
-trees_hit = 0
-current_pos = 0
-right_incrementor = 3
-down_incrementor = 1
-
-lines.each do |line|
-  chars = line.split(//)
-  if chars[current_pos] == '#'
-    puts 'hit tree: ' + chars[current_pos]
-    trees_hit += 1
-  else
-    puts 'WHEEEE: ' + chars[current_pos]
+def skiiii(right_incrementor, down_incrementor, lines, output = true)
+  trees_hit = 0
+  current_pos = 0
+  current_down_count = down_incrementor # start from first position
+  puts "right #{right_incrementor}, down #{down_incrementor}" if output
+  lines.each do |line|
+    if current_down_count == down_incrementor
+      chars = line.split(//)
+      if chars[current_pos] == '#'
+        puts 'hit tree: ' + chars[current_pos] if output
+        trees_hit += 1
+      else
+        puts 'WHEEEE: ' + chars[current_pos] if output
+      end
+      current_pos += right_incrementor
+      current_down_count = 1
+    else
+      current_down_count += 1
+    end
   end
-  current_pos += right_incrementor
+  trees_hit
 end
 
-puts 'trees hit:'
-puts trees_hit
+puts "answer 1: #{skiiii(3, 1, lines, false)}"
+
+sets = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+]
+answers = []
+sets.each do |set|
+  trees = skiiii(set[0], set[1], lines, output = true)
+  answers.append trees
+  puts "trees hit: #{trees}"
+  puts ''
+end
+
+multi = ''
+answers.each do |n|
+  multi = if multi == ''
+            n
+          else
+            multi * n
+          end
+end
+puts "answer 2: #{multi}"
